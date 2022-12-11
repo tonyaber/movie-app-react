3ce74817c9d3619979058c9fa9fc6d7b0581c058
unknown <tonyaberch@gmail.com>
2022-12-11 16:16:43 +0100

    deploy

--- src/spinner/spinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./spinner.css";
-
-export default function LoadingSpinner() {
-  return (
-    <div className={style["loader"]}>Loading...</div>
-  );
-}
